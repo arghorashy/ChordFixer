9389463911e4a18d9fb53dff74453fee14f61f9f
Afsheen Ghorashy <arghorashy@gmail.com>
2014-10-26 21:33:55 -0400

    Can grab GuitarHero chords now!

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ javascriptFiles["GH"] = "processGH.js"
 // Show Action Page icon and bind click, if necessary
 function preparePageAction( tabId, changeInfo, tab ) {
 
-	pass = false;
-	type = "";
+	var pass = false;
+	var type = "";
 
 	Object.keys(supportedPages).forEach(
 		function (key)
@@ -33,7 +33,7 @@ function preparePageAction( tabId, changeInfo, tab ) {
 	{
 		// Show Action Page icon
 		chrome.pageAction.show(tabId);
-		addOnClickListener(type);
+		addOnClickListener(type, tabId, changeInfo, tab);
 
 
     }
@@ -41,22 +41,44 @@ function preparePageAction( tabId, changeInfo, tab ) {
 
 
 
-function addOnClickListener(type)
+function addOnClickListener(type, tabId, changeInfo, tab)
 {
-	chrome.pageAction.onClicked.addListener(
-		function (tab) { //Fired when User Clicks ICON
-	        chrome.tabs.executeScript(tab.id, {"file": javascriptFiles[type]}, 
-	        	function () 
-	        	{ // Execute your code
-	            	console.log(javascriptFiles[type] + " executed .. "); // Notification on Completion
-	        	});
-	    
-	});	
+	if (tab.url !== undefined && changeInfo.status == "complete")
+	{
+		chrome.pageAction.onClicked.addListener(
+			function (tab) { //Fired when User Clicks ICON
+		        chrome.tabs.executeScript(tab.id, {"file": javascriptFiles[type]}, 
+		        	function () 
+		        	{ // Execute your code
+		            	console.log(javascriptFiles[type] + " executed .. "); // Notification on Completion
+		        	});
+			});
+	}
+
 
 	console.log("Bound click with " + javascriptFiles[type] + ".");
 }
 
+function addDebuggingConnection()
+{
+
+	chrome.extension.onRequest.addListener(
+		function (request, sender, sendResponse) 
+		{	
+			console.log("hihihihi");
+			if (typeof(request) == "string")
+			{
+				console.log("hihihihi");
+				// console.log("<Message from "+sender.url+">\n"+request+"\n</Message from "sender.url+">\n");
+				sendResponse(true);
+			}
+			else sendResponse(false);
+		});
+}
 
 // Determine whether the Action Page icon should be shown every time a page is loaded
 chrome.tabs.onUpdated.addListener(preparePageAction);
+
+// Debugging connection with content scripts
+addDebuggingConnection();
 
---
+++ processGH.js
@@ -0,0 +1,50 @@
+
+function getChordContainer()
+{
+	var $elem = $(".tb_ct");
+	if ($elem.length != 1) 
+	{
+		alert("Could not process webpage. \n\nDeveloper Note: The number of HTML elements of class \"tb_ct\" in page was not exactly 1.");
+		return;
+	}
+
+	$elem = $("#cont", $elem);
+	if ($elem.length != 1) 
+	{
+		alert("Could not process webpage. \n\nDeveloper Note: the number of HTML elements of id \"cont\" under div.tb_ct in page was not exactly 1.");
+		return;
+	}
+
+	// Need to fix the dev note here!!!
+	$elem = $("pre[style*='font-size:']:not('.print-visible')", $elem);
+
+	if ($elem.length != 1) 
+	{
+		alert("Could not process webpage. "+$elem.length+" \n\nDeveloper Note: the number of pre HTML elements of under div.cont in page was less than 2.");
+		//return;
+	}
+
+	return $elem;
+
+}
+
+function processChordPage()
+{
+	var $chordContainer = getChordContainer();
+
+	if ($chordContainer != undefined)
+	{
+		console.log("hahahah");
+	}
+
+}
+
+function debugLog(str)
+{
+	chrome.extension.sendRequest(str, null);	
+}
+
+processChordPage();
+
+
+
